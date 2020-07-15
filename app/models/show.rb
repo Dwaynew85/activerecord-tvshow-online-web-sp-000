173b5b746702cb52f070233ed0e4f0e0838ec605
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-15 05:03:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   def highest_rating
     #return the highest value in the ratings column. hint: use maximum
-    Show.maximum(:rating)
+    Show.maximum(rating)
   end
 
   def most_popular_show
