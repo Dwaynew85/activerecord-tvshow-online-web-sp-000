b007d158d81d5d84e18d5a48f3c6fc3f8aa49815
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-15 05:11:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,29 @@
 class Show < ActiveRecord::Base
-  def highest_rating
-    #return the highest value in the ratings column. hint: use maximum
+  def Show::highest_rating
     Show.maximum("rating")
   end
 
-  def most_popular_show
+  def Show::most_popular_show
     #return the show with the highest rating. hint: use highest_rating as helper method
   end
 
-  def lowest_rating
+  def Show::lowest_rating
     #returns lowest value in the ratings column
   end
 
-  def least_popular_show
+  def Show::least_popular_show
     #returns the show with the lowest rating
   end
 
-  def ratings_sum
+  def Show::ratings_sum
     #return the sum of all of the ratings
   end
 
-  def popular_shows
+  def Show::popular_shows
     # returns an array of all the shows that have a rating greater than 5. hint: use where
   end
 
-  def shows_by_alphabetical_order
+  def Show::shows_by_alphabetical_order
     #returns array of all the shows sorted in alphabetical order by name. hint: use order
   end
 end
